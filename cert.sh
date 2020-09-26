69e3b8aff3f980c18944d1e430c94ae051f37408
Sean Chao <seanchao0804@gmail.com>
2020-09-26 15:32:35 +0000

    fix: mkdir trojan-crt

--- cert.sh
+++ cert.sh
@@ -18,4 +18,6 @@ fi
 echo -e "Please make sure port 80 is not occupied by other programs"
 echo -e "Install TLS certificate for \033[0;34m$domain\033[;0m"
 ~/.acme.sh/acme.sh --issue -d ${domain} --standalone -k ec-256
+
+mkdir -p ./trojan-crt
 ~/.acme.sh/acme.sh --installcert -d ${domain} --fullchainpath ./trojan-crt/server.crt --keypath ./trojan-crt/server.key --ecc
