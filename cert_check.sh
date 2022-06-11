30c24255f9a899944b0c7645362879d273faa410
SeanChao <seanchao0804@gmail.com>
2022-06-11 14:17:38 +0800

    feat: auto restart

--- cert_check.sh
+++ cert_check.sh
@@ -1,4 +1,5 @@
-exp_date=$(date -d "$(openssl x509 -enddate -noout -in ./cert/fullchain.crt | awk -F = '{print $2}')" '+%s')
+CERT_PATH=$1
+exp_date=$(date -d "$(openssl x509 -enddate -noout -in $CERT_PATH/fullchain.crt | awk -F = '{print $2}')" '+%s')
 now=$(date '+%s')
 echo "EXPIRE AT: ${exp_date}"
 echo "NOW: ${now}"
