6ee25780b28a299d60a597fc2fcb77e3c33d05fa
SeanChao <seanchao0804@gmail.com>
2023-08-13 14:30:42 +0100

    update xray config and cert script

--- cert.sh
+++ cert.sh
@@ -2,11 +2,12 @@
 set -e
 
 # Parse domain from shell param
-if [[ $# != 1 ]]; then
-    echo "Usage: $0 your-domain.com"
+if [[ $# < 1 ]]; then
+    echo "Usage: $0 your-domain.com [acme-issue-args]"
     exit 1
 fi
 domain=$1
+acme_issue_args=${2:-""}
 
 # Install acme.sh
 ACME=~/.acme.sh/acme.sh
@@ -23,7 +24,7 @@ echo -e "Install TLS certificate for \033[0;34m$domain\033[;0m"
 set -o allexport; source cert.env; set +o allexport
 
 $ACME --register-account -m ${ZERO_SSL_EMAIL}
-$ACME --issue -d ${domain} -k ec-256 --dns dns_cf
+$ACME --issue -d ${domain} -k ec-256 --dns dns_cf --force ${acme_issue_args}
 
 CERT_DIR=./cert
 mkdir -p ${CERT_DIR}
--- cert_check.sh
+++ cert_check.sh
@@ -1,6 +1,34 @@
+#!/bin/bash
+
+# Exit if no parameter is passed
+if [[ $# != 1 ]]; then
+    echo "Usage: $0 cert_path"
+    exit 1
+fi
+
+# Set the certificate path
 CERT_PATH=$1
+
+# Check if the certificate exists
+if [ ! -f $CERT_PATH/fullchain.crt ]; then
+    echo "Certificate not found at $CERT_PATH"
+    exit 1
+fi
+
+# Get the expiration date of the certificate
 exp_date=$(date -d "$(openssl x509 -enddate -noout -in $CERT_PATH/fullchain.crt | awk -F = '{print $2}')" '+%s')
+
+# Get the current date
 now=$(date '+%s')
-echo "EXPIRE AT: ${exp_date}"
-echo "NOW: ${now}"
-test $exp_date -gt $now
+
+# Print the expiration date and the current date
+echo "EXP: $(date -d @$exp_date '+%Y-%m-%d %H:%M:%S')"
+echo "NOW: $(date -d @$now '+%Y-%m-%d %H:%M:%S')"
+
+# Check if the certificate is expired
+if test $exp_date -gt $now; then
+    echo "✅ Certificate is OK"
+else
+    echo -e "❌ \033[0;31mCertificate is expired\033[0m"
+    exit 1
+fi
